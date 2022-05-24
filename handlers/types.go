e6a779a1c9029dea2b762bd39cf261bc138191df
Brandon Rubio <brubio3724@gmail.com>
2022-05-24 14:49:06 -0400

    Added PlayerStats struct

--- handlers/types.go
+++ handlers/types.go
@@ -1,21 +1,33 @@
 package handlers
 
 type Country struct {
-	Name string
-	Flag string
+	Name string `json:"name"`
+	Flag string `json:"flag"`
 }
 
+// Used with team and teams
 type Player struct {
-	Fullname string
-	Image    string
-	Nickname string
-	Country  Country
+	Fullname string  `json:"fullname"`
+	Image    string  `json:"image"`
+	Nickname string  `json:"nickname"`
+	Country  Country `json:"country"`
 }
 
 type Team struct {
-	Id      int
-	Ranking int
-	Name    string
-	Logo    string
-	Players []Player
+	Id      int      `json:"id"`
+	Ranking int      `json:"ranking"`
+	Name    string   `json:"name"`
+	Logo    string   `json:"logo"`
+	Players []Player `json:"players"`
+}
+
+// Used with player and players
+type PlayerStats struct {
+	Id         int    `json:"id"`
+	Nickname   string `json:"nickname"`
+	Team       string `json:"team"`
+	Slug       string `json:"slug"`
+	MapsPlayed string `json:"mapsPlayed"`
+	Kd         string `json:"kd"`
+	Rating     string `json:"rating"`
 }
