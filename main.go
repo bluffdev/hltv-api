f6c49f530671d697acee98df27865a6ad4643996
Brandon Rubio <brubio3724@gmail.com>
2022-06-01 15:47:26 -0400

    Added port env variable

--- main.go
+++ main.go
@@ -4,17 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/landoniwnl/hltv-api/routes"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	
+	if port == "" {
+		port = "3000"
+	}
+
     router := mux.NewRouter()
     
     routes.RegisterRoutes(router)
     http.Handle("/", router)
 
-    fmt.Println("Listening on port 3000")
-    log.Fatal(http.ListenAndServe("localhost:3000", router))
+    fmt.Println("Listening on port " + port)
+    log.Fatal(http.ListenAndServe(":" + port, router))
 }
